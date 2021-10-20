118f164a7a85aa643a0aeae5f1338c6428d0efef
alekseiBoiko <mrboikoaleksei@gmail.com>
2021-10-20 19:13:51 +0300

    styling

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 $(function(){
   fixHeader();
   changeHamburger();
-  initHolderBg('.welcome');
+  initRaty('.review__raty');
   drawSlider();
   }
 );
@@ -37,36 +37,27 @@ function changeHamburger() {
 };
 
 
-function initHolderBg(holder) {
-  "use strict";
-  // set image as inline background
-  $(holder).each(function() { 
-      var imgHolder = $(this),
-              img = imgHolder.find('img'),
-              imgSrc = img.attr('src');
-      if(!!imgHolder.length && !!img.length && !!imgSrc) {
-          imgHolder.css({
-              backgroundImage: 'url(' + imgSrc + ')',
-              backgroundSize: 'cover',
-              backgroundPosition: '50% 50%',
-              backgroundRepeat: 'no-repeat',
-              // height: '100%'
-          });
-          img.remove();
-      }
+function initRaty(starClass) {
+  'use strict';
+  $(starClass).raty({
+    score: 5,
+    starOff: 'images/star-off.png',
+    starOn: 'images/star-on.png',
   });
 }
 
 
 function drawSlider() {
-  $('.offer-list').slick({
+  $('.review-list').slick({
       
-      slidesToShow: 5,
+      slidesToShow: 2,
       infinite: true,
       dots: true,
-      slidesToScroll: 2,
+      slidesToScroll: 1,
       slide: 'li',
-      arrows: false,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+      nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
       responsive: [
         {
           breakpoint: 1200,
